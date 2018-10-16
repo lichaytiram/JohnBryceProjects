10c8654db1ee6b58e87471a47757c50ddc3325fe
lichaytiram <lichaytiram@gmail.com>
2018-10-16 22:46:44 +0300

    java

--- Practice/src/try71.java
+++ Practice/src/try71.java
@@ -3,6 +3,19 @@ public class try71 {
 
 	public static void main(String[] args) {
 
+		String s1 = "  hi-o m      glol     ";
+		System.out.println(s1);
+		s1 = s1.trim();
+		System.out.println(s1);
+		String s = "abcdE";
+		System.out.println(s + "\t" + "hi");
+		String s2 = "abcc", s3 = "Abc";
+		System.out.println(s2.compareTo(s3));
+		s3 = s3.toLowerCase();
+		System.out.println(s2.compareTo(s3));
+		System.out.println(s3.concat(" "));
+		
+		
 	}
 
 }
