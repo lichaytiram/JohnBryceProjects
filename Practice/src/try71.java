ffe256bee24292aaf9c2c1f78b93a64f2e256eed
lichaytiram <lichaytiram@gmail.com>
2018-10-16 23:09:37 +0300

    java

--- Practice/src/try71.java
+++ Practice/src/try71.java
@@ -14,6 +14,11 @@ public class try71 {
 		s3 = s3.toLowerCase();
 		System.out.println(s2.compareTo(s3));
 		System.out.println(s3.concat(" "));
+		char[] c = { 'c', 'r', 'a', 'b', 'c' };
+		System.out.println(String.copyValueOf(c, 0, 4));
+		String s4 = "abcd";
+		String s5 = "bcd";
+		System.out.println(s4.endsWith(s5));
 		
 		
 	}
