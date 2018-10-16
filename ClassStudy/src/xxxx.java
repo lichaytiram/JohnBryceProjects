234ddf72107e33e89e14dc2b050600c28b850313
lichaytiram <lichaytiram@gmail.com>
2018-10-16 22:49:35 +0300

    java

--- ClassStudy/src/xxxx.java
+++ ClassStudy/src/xxxx.java
@@ -4,5 +4,6 @@ public class try1 {
 	public static void main(String[]args) {
 		int x=5,y=10;
 		System.out.println(x*y);
+		
 	}
 }
--- Practice/src/try72.java
+++ Practice/src/try72.java
@@ -3,6 +3,7 @@ public class try72 {
 
 	public static void main(String[] args) {
 
+		
 	}
 
 }
