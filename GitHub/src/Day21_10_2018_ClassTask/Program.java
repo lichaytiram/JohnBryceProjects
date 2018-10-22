c0916b51a0dc34e421717469e9da1db84149f2f2
lichaytiram <lichaytiram@gmail.com>
2018-10-23 02:36:54 +0300

    java

--- GitHub/src/Day21_10_2018_ClassTask/Program.java
+++ GitHub/src/Day21_10_2018_ClassTask/Program.java
@@ -1,14 +1,6 @@
 package Day21_10_2018_ClassTask;
 
 public class Program {
-	public static IFly createIFly() {
-		int r = (int) (Math.random() * 3);
-		if (r == 0)
-			return new Bird();
-		else if (r == 1)
-			return new Pilot();
-		return new Kite();
-	}
 
 	public static void main(String[] args) {
 		IFly[] ifly = new IFly[10];
@@ -21,5 +13,14 @@ public class Program {
 			System.out.println(temp.land());
 			System.out.println();
 		}
+	}
+
+	public static IFly createIFly() {
+		int r = (int) (Math.random() * 3);
+		if (r == 0)
+			return new Bird();
+		else if (r == 1)
+			return new Pilot();
+		return new Kite();
 	}
 }
