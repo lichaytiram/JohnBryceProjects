2cc799d70c7f9d5c7f0b147a5a5e0b008e0bdc93
Lichay <Lichay@192.168.1.18>
2018-10-26 22:39:00 +0300

    java

---
+++ GitHub/src/Day24_10_2018_Project2/Program.java
@@ -0,0 +1,7 @@
+package Day24_10_2018_Project2;
+
+public class Program {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ GitHub/src/Day24_10_2018_Project2/SimpleTime.java
@@ -0,0 +1,5 @@
+package Day24_10_2018_Project2;
+
+public class SimpleTime {
+
+}
