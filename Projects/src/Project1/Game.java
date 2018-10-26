b5db83960d64a77b8406360fdc5bf894445a5252
Lichay <Lichay@192.168.1.18>
2018-10-26 13:32:41 +0300

    java

--- Projects/src/Project1/Game.java
+++ Projects/src/Project1/Game.java
@@ -25,25 +25,39 @@ public class Game {
 	}
 
 	public void getIndex(char x, int row, int col) {
-		print();
 		Scanner scan = new Scanner(System.in);
+		while (row > 2) {
+			System.out.println("row out of range");
+			row = scan.nextInt();
+		}
+		while (col > 2) {
+			System.out.println("col out of range");
+			col = scan.nextInt();
+		}
 		if (this.c[row][col] <= '9' && this.c[row][col] >= '1')
 			this.c[row][col] = x;
 		else {
 			System.out.println("The place is alredy takken!\nChoose a new row and col");
 			getIndex(x, scan.nextInt(), scan.nextInt());
 		}
-		scan.close();
+
 		print();
+		if (isWin)
+			scan.close();
+		win(x);
+
 	}
 
 	public void win(char x) {
-		if ((c[0][0] == x && c[0][1] == x && c[0][2] == x) || (c[1][0] == x && c[1][1] == x && c[1][2] == x))
-			isWin = true;
-
+		for (int i = 0; i < c.length; i++) {
+			if ((c[i][0] == x && c[i][1] == x && c[i][2] == x) || (c[0][i] == x && c[1][i] == x && c[2][i] == x)
+					|| (c[0][0] == x && c[1][1] == x && c[2][2] == x) || (c[0][2] == x && c[1][1] == x && c[2][0] == x))
+				isWin = true;
+		}
 	}
 
 	private void print() {
+		System.out.println();
 		for (int i = 0; i < c.length; i++) {
 			for (int j = 0; j < c[i].length; j++) {
 				if (j == 0)
--- Projects/src/Project1/Program.java
+++ Projects/src/Project1/Program.java
@@ -8,15 +8,29 @@ public class Program {
 		User u1 = new User('x');
 		User u2 = new User('o');
 		Game g1 = new Game();
+		Game g2 = new Game();
+		gameplay(u1, u2, g1, scan);
+		System.out.println("second game");
+		gameplay(u1, u2, g2, scan);
+		scan.close();
+	}
+
+	public static void gameplay(User u1, User u2, Game g1, Scanner scan) {
 		while (!g1.getiswin()) {
 			System.out.println("choose row and column for user 1");
 			g1.getIndex(u1.getC(), scan.nextInt(), scan.nextInt());
-			if (g1.getiswin())
+			if (g1.getiswin()) {
+				u1.setWin();
+				System.out.println("The winer is " + u1.getC() + " and won " + u1.getWin() + " times!");
 				break;
-
+			}
 			System.out.println("choose row and column for user 2");
 			g1.getIndex(u2.getC(), scan.nextInt(), scan.nextInt());
+			if (g1.getiswin()) {
+				u2.setWin();
+				System.out.println("The winer is " + u2.getC() + " and won " + u2.getWin() + " times!");
+				break;
+			}
 		}
-		scan.close();
 	}
 }
