0256848abf6c1c62801970a4b52e8166c2c4739d
Lichay <Lichay@192.168.1.14>
2018-11-03 18:28:31 +0200

    java

--- Practice/src/Program20/Program.java
+++ Practice/src/Program20/Program.java
@@ -11,7 +11,7 @@ public class Program {
 		System.out.println(z.addAnimal(bear2));
 		System.out.println(z.addAnimal(dragon1));
 		System.out.println(z.addAnimal(dragon2));
-		System.out.println(z+"\n");
+		System.out.println(z + "\n");
 		for (int i = 0; z.getAnimal(i) != null; i++) {
 			if (z.getAnimal(i) instanceof bearCare)
 				System.out.println(((bearCare) z.getAnimal(i)).getSign());
