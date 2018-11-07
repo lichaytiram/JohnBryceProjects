b135aa2eb806559ea9bee43be58cdf64a8fa6eba
Lichay <Lichay@DESKTOP-K6P3KUO>
2018-11-07 21:06:21 +0200

    java

--- Practice/src/try73.java
+++ Practice/src/try73.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class try73 {
 	public static void main(String[] args) {
@@ -6,5 +7,30 @@ public class try73 {
 		System.out.println(s1);
 		System.out.println(s1.compareTo(s2));
 		System.out.println(s1);
+
+		char[] cArr = { 'a', 'b', 'c', 'd', 'e', 'f', 'g' };
+		String s = "";
+		System.out.println(s.length());
+		s = String.copyValueOf(cArr, 0, 3);
+		System.out.println(s);
+		System.out.println(s.length());
+
+		String s3 = "omge1";
+		String s4 = "omge1";
+		System.out.println(s3.endsWith(s4));
+		String tryString = "hiWorlds";
+		char[] arrc = new char[5];
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		System.out.println(list);
+		list.add(5);
+		System.out.println(list.size());
+		list.add(0, 6);
+		list.add(7);
+		System.out.println(list.size());
+		System.out.println(list);
+
+		list.remove(1);
+		System.out.println(list);
 	}
 }
