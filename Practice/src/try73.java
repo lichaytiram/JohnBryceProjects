084b9031bd95bf50e0d104b82bad3a679e098eb3
Lichay <Lichay@192.168.1.14>
2018-11-07 07:19:39 +0200

    java

--- Practice/src/try73.java
+++ Practice/src/try73.java
@@ -1,6 +1,10 @@
 
 public class try73 {
 	public static void main(String[] args) {
-
+		String s1 = "abcD -a";
+		String s2 = "abcD -e";
+		System.out.println(s1);
+		System.out.println(s1.compareTo(s2));
+		System.out.println(s1);
 	}
 }
