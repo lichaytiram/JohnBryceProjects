77febab37c675cd0102aac4921d4d9e219deab1b
Lichay <Lichay@192.168.1.15>
2018-11-12 21:21:34 +0200

    java

--- Practice/src/try77.java
+++ Practice/src/try77.java
@@ -1,15 +1,18 @@
+import java.util.Arrays;
 
 public class try77 {
 	public static void main(String[] args) {
 		String s = "abcdecfghcijk";
 		System.out.println(s.indexOf('c', 2));
 		System.out.println(s.indexOf('c'));
-		System.out.println(s.lastIndexOf('c',8));
-		
+		System.out.println(s.lastIndexOf('c', 8));
+
 		System.out.println(s.lastIndexOf('c', 8));
 		String[][] s1 = new String[5][]; // check?
+		System.out.println(s1);
 		System.out.println();
 
 		String[][] sTry1 = { { "4", "f" }, { "f" } };
+		System.out.println(Arrays.deepToString(sTry1));
 	}
 }
