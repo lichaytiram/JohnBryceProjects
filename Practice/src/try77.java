98c44295d3beb5dc8266a73681b5e77863136efa
Lichay <Lichay@192.168.1.15>
2018-11-12 21:19:04 +0200

    java

--- Practice/src/try77.java
+++ Practice/src/try77.java
@@ -4,15 +4,12 @@ public class try77 {
 		String s = "abcdecfghcijk";
 		System.out.println(s.indexOf('c', 2));
 		System.out.println(s.indexOf('c'));
-<<<<<<< HEAD
 		System.out.println(s.lastIndexOf('c',8));
 		
-=======
 		System.out.println(s.lastIndexOf('c', 8));
 		String[][] s1 = new String[5][]; // check?
 		System.out.println();
 
 		String[][] sTry1 = { { "4", "f" }, { "f" } };
->>>>>>> branch 'master' of https://github.com/lichaytiram/JohnBryceProjects.git
 	}
 }
