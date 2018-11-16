5f4121d540bbae8836e5c48cd6a035db768d6027
Lichay <Lichay@192.168.1.15>
2018-11-16 23:21:38 +0200

    java

--- Practice/src/try87.java
+++ Practice/src/try87.java
@@ -7,10 +7,10 @@ public class try87 {
 		listbinary.add(1);
 		listbinary.add(2);
 		listbinary.add(5);
-		System.out.println(ConvertBinaryArrayToInt(listbinary));
+		System.out.println(ConvertInttoBinaryArray(listbinary));
 	}
 
-	public static int ConvertBinaryArrayToInt(ArrayList<Integer> binary) {
+	public static int ConvertInttoBinaryArray(ArrayList<Integer> binary) {
 		int inti = 0;
 		for (int i = 0; i < binary.size(); i++) {
 			inti *= 10;
