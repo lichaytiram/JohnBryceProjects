dc494b14789648f74b0d903de5fe8e32479ed68c
Lichay <Lichay@192.168.1.13>
2018-12-01 15:34:12 +0200

    SQL

--- PracticeSQL/src/Program1/Program.java
+++ PracticeSQL/src/Program1/Program.java
@@ -1,10 +1,12 @@
 package Program1;
 
 public class Program {
-	
+	private static String USERNAME = "root";
+	private static String PASSWORD = null;
+	private static String URL = "jdbc:mysql://localhost:3306/JB";
+
 	public static void main(String[] args) {
-		
+
 	}
-	
-	
+
 }
