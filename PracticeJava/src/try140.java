0f86334ec9163fe5926337f5145e34ad9429f73c
Lichay <Lichay@192.168.1.16>
2018-12-06 00:57:26 +0200

    java

--- PracticeJava/src/try140.java
+++ PracticeJava/src/try140.java
@@ -1,15 +1,16 @@
 
 public class try140 {
 	public static void main(String[] args) {
-		System.out.println(random());
+		System.out.println(random(new String[] { "Lichay", "Dor", "Maor", "Nil", "Talia" }));
 	}
-	public static String random() {
-		String[] project = { "Lichay", "Dor", "Maor", "Nil", "Talia" };
+
+	public static String random(String[] project) {
 		String values = "Team 1: ";
+		int length = project.length;
 		for (int i = 0; i < 3; i++) {
-			int rand = rand();
+			int rand = rand(length);
 			while (values.contains(project[rand]))
-				rand = rand();
+				rand = rand(length);
 			values += project[rand] + " ";
 		}
 		values += "\nTeam 2: ";
@@ -19,7 +20,8 @@ public class try140 {
 		return values;
 
 	}
-	public static int rand() {
-		return (int) (Math.random() * 5);
+
+	public static int rand(int n) {
+		return (int) (Math.random() * n);
 	}
 }
