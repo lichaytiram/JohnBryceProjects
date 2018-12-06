f8cf82b8688bcebd9517be34e476c76c108b9e45
Lichay <Lichay@192.168.1.16>
2018-12-06 12:26:54 +0200

    SQL

--- PracticeSQL/src/Program1/Program.java
+++ PracticeSQL/src/Program1/Program.java
@@ -6,7 +6,7 @@ public class Program {
 	private static String USERNAME = "root";
 	private static String PASSWORD = null;
 
-	private static String URL = "jdbc:mysql://localhost:3306/?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	private static String URL = "jdbc:mysql://localhost:3306/test?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 
 	public static void main(String[] args) {
 		try {
@@ -19,11 +19,10 @@ public class Program {
 
 	public static void create() throws SQLException {
 		Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-		System.out.println("hh");
 		con.createStatement()
-				.executeUpdate("CREATE TABLE IF NOT EXSIST person" + "(lable int(5) auto_increments not null,"
+				.executeUpdate("CREATE TABLE IF NOT EXISTS de" + "(lable int(5) not null auto_increment,"
 						+ "name varchar(10) not null," + "id int(9) not null," + "age int(3) default null,"
-						+ "lable primary");
+						+ "primary key(lable))");
 		System.out.println("table is succeed");
 	}
 
