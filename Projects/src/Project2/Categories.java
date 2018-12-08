10bd49591e96c6137a64ee7141af1293dc1e011d
Lichay <Lichay@192.168.1.15>
2018-12-08 14:32:34 +0200

    project

---
+++ Projects/src/Project2/Categories.java
@@ -0,0 +1,47 @@
+package Project2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Categories {
+	public static void create() throws SQLException {
+		Connection con = DriverManager.getConnection(Program.URL, Program.USERNAME, Program.PASSWORD);
+		con.createStatement().executeUpdate(
+				"CREATE TABLE IF NOT EXISTS categories (ID INT(255) NOT NULL AUTO_INCREMENT ,NAME VARCHAR(10) NOT NULL,PRIMARY KEY(ID))");
+		System.out.println("The table categories has created");
+	}
+
+	public static void drop() throws SQLException {
+		Connection con = DriverManager.getConnection(Program.URL, Program.USERNAME, Program.PASSWORD);
+		con.createStatement().executeUpdate("DROP TABLE categories");
+		System.out.println("The table categories is a drop");
+	}
+
+	public static void insert(String Name) throws SQLException {
+		Connection con = DriverManager.getConnection(Program.URL, Program.USERNAME, Program.PASSWORD);
+		con.createStatement().executeUpdate("insert into categories (NAME) values ('" + Name + "')");
+		System.out.println("insert categories has succeed");
+	}
+
+	public static void delete(int indexToDelete) throws SQLException {
+		Connection con = DriverManager.getConnection(Program.URL, Program.USERNAME, Program.PASSWORD);
+		con.createStatement().executeUpdate("DELETE FROM categories WHERE ID=" + indexToDelete);
+		System.out.println("delete from categories has done");
+	}
+
+	public static void update(String Name, int index) throws SQLException {
+		Connection con = DriverManager.getConnection(Program.URL, Program.USERNAME, Program.PASSWORD);
+		con.createStatement().executeUpdate("UPDATE categories SET NAME='" + Name + "' WHERE ID=" + index);
+		System.out.println("update categories has done");
+	}
+
+	public static void showAll() throws SQLException {
+		Connection con = DriverManager.getConnection(Program.URL, Program.USERNAME, Program.PASSWORD);
+		ResultSet re = con.createStatement().executeQuery("SELECT * FROM categories");
+		while (re.next())
+			System.out.println("ID: " + re.getInt("ID") + " ,NAME: " + re.getString("NAME"));
+	}
+
+}
--- Projects/src/Project2/Customers.java
+++ Projects/src/Project2/Customers.java
@@ -2,6 +2,7 @@ package Project2;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Customers {
@@ -9,13 +10,43 @@ public class Customers {
 		Connection con = DriverManager.getConnection(Program.URL, Program.USERNAME, Program.PASSWORD);
 		con.createStatement().executeUpdate(
 				"CREATE TABLE IF NOT EXISTS customers (ID INT(255) NOT NULL AUTO_INCREMENT ,FIRST_NAME VARCHAR(10) NOT NULL,lAST_NAME VARCHAR(10) DEFAULT NULL,EMAIL VARCHAR(25) DEFAULT NULL,PASSWORD VARCHAR(50) NOT NULL,PRIMARY KEY(ID))");
-		System.out.println("The table Customers has created");
+		System.out.println("The table customers has created");
+	}
+
+	public static void drop() throws SQLException {
+		Connection con = DriverManager.getConnection(Program.URL, Program.USERNAME, Program.PASSWORD);
+		con.createStatement().executeUpdate("DROP TABLE customers");
+		System.out.println("The table customers is a drop");
 	}
 
 	public static void insert(String firstName, String lastName, String email, String password) throws SQLException {
 		Connection con = DriverManager.getConnection(Program.URL, Program.USERNAME, Program.PASSWORD);
 		con.createStatement().executeUpdate("insert into customers (FIRST_NAME,lAST_NAME,EMAIL,PASSWORD) values ('"
 				+ firstName + "','" + lastName + "','" + email + "','" + password + "')");
-		System.out.println("insert has succeed");
+		System.out.println("insert customers has succeed");
 	}
+
+	public static void delete(int indexToDelete) throws SQLException {
+		Connection con = DriverManager.getConnection(Program.URL, Program.USERNAME, Program.PASSWORD);
+		con.createStatement().executeUpdate("DELETE FROM customers WHERE ID=" + indexToDelete);
+		System.out.println("delete from customers has done");
+	}
+
+	public static void update(String firstName, String lastName, String email, String passWord, int index)
+			throws SQLException {
+		Connection con = DriverManager.getConnection(Program.URL, Program.USERNAME, Program.PASSWORD);
+		con.createStatement().executeUpdate("UPDATE companies SET FIRST_NAME='" + firstName + "',lAST_NAME='" + lastName
+				+ "', EMAIL='" + email + "', PASSWORD='" + passWord + "' WHERE ID=" + index);
+		System.out.println("update customers has done");
+	}
+
+	public static void showAll() throws SQLException {
+		Connection con = DriverManager.getConnection(Program.URL, Program.USERNAME, Program.PASSWORD);
+		ResultSet re = con.createStatement().executeQuery("SELECT * FROM customers");
+		while (re.next())
+			System.out.println("ID: " + re.getInt("ID") + " ,FIRST_NAME: " + re.getString("FIRST_NAME")
+					+ " ,lAST_NAME: " + re.getString("lAST_NAME") + " ,EMAIL: " + re.getString("EMAIL") + " ,PASSWORD:"
+					+ re.getString("PASSWORD"));
+	}
+
 }
