3c3adcf60f8307f4181b279f96388e3582712dba
Lichay <Lichay@192.168.1.15>
2018-12-12 00:03:12 +0200

    Project

--- Projects/src/Project2/ConnectionPool.java
+++ Projects/src/Project2/ConnectionPool.java
@@ -7,7 +7,8 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class ConnectionPool {
-
+	// don't finish yet
+	// singleton ConnectionPool -->
 	private static String USERNAME = "root";
 	private static String PASSWORD = null;
 	private final static String URL = "jdbc:mysql://localhost:3306/test?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
