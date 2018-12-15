5f9385bdf9067f49e4ef39a7f80a67d378638eb1
Lichay <Lichay@192.168.1.11>
2018-12-15 20:37:41 +0200

    Project

--- ProjectJB/src/BaseProgram/mainDemo.java
+++ ProjectJB/src/BaseProgram/mainDemo.java
@@ -10,8 +10,8 @@ public class mainDemo {
 			Customer cu1 = new Customer(5, "45sD64", "lichay@s.c", "lichay", "soos");
 			System.out.println(c1);
 			System.out.println(cu1);
-			c1.getCouponList().add(new Coupon());
-			c1.getCouponList().add(new Coupon());
+//			c1.getCouponList().add(new Coupon());
+//			c1.getCouponList().add(new Coupon());
 			System.out.println(c1);
 		} catch (ExceptionName e) {
 			System.out.println(e);
