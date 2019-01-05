848cd000c0b4dce18a6ac1f752d4a7ece95ee243
lichay <lichaytiram@gmail.com>
2019-01-05 22:33:46 +0200

    java

---
+++ PracticeJava/src/try143.java
@@ -0,0 +1,19 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class try143 {
+	public static void main(String[] args) {
+
+		String name = "myxe name is lichay!%$ 100x";
+		String p = "(\\D)";
+
+		Pattern check = Pattern.compile(p);
+		Matcher m = check.matcher(name);
+
+		if (m.find()) {
+			System.out.println("Found value: " + m.group(0));
+			System.out.println("Found value: " + m.group(1));
+		}
+
+	}
+}
