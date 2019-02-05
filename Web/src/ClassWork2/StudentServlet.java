6c2b9c92c837f43b2dd9184ceb2bb47ce550b159
lichay <lichaytiram@gmail.com>
2019-02-05 19:12:45 +0200

    web

---
+++ Web/src/ClassWork2/StudentServlet.java
@@ -0,0 +1,65 @@
+package ClassWork2;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import ClassWork2.StudentList.Student;
+
+/**
+ * Servlet implementation class StudentServlet
+ */
+@WebServlet("/StudentServlet")
+public class StudentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public StudentServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+
+	@SuppressWarnings("unchecked")
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		JSONArray jsonArray = new JSONArray();
+
+		for (Student student : StudentList.getInstance().list) {
+			JSONObject object = new JSONObject();
+			object.put("id", student.getId());
+			object.put("name", student.getName());
+			object.put("age", student.getAge());
+			object.put("grade", student.getGrade());
+			jsonArray.add(object);
+		}
+
+		response.getWriter().append(jsonArray.toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
