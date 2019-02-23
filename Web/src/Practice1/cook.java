e77a89b3f4cca1a4c81f4592dcf2d74e54697ebc
lichay <lichaytiram@gmail.com>
2019-02-23 22:46:04 +0200

    web

--- Web/src/Practice1/cook.java
+++ Web/src/Practice1/cook.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.catalina.tribes.util.Arrays;
 
 /**
  * Servlet implementation class cook
