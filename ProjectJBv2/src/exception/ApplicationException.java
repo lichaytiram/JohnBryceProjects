0fd6e0be38146fcf82d14e0c2845b9782eef1c10
Lichay Tiram <lichaytiram@gmail.com>
2019-04-10 21:38:21 +0300

    project

--- ProjectJBv2/src/exception/ApplicationException.java
+++ ProjectJBv2/src/exception/ApplicationException.java
@@ -1,5 +1,7 @@
 package exception;
 
+import enums.ErrorType;
+
 /**
  * This class create a new exception
  * 
@@ -9,19 +11,32 @@ package exception;
 public class ApplicationException extends Exception {
 
 	private static final long serialVersionUID = -1835434097622353495L;
+	private ErrorType errorType;
 
 	/**
 	 * @param problem   Receive a problem
 	 * @param throwable Receive addition exception
 	 */
 	public ApplicationException(String problem, Throwable throwable) {
+
 		super(problem, throwable);
+
 	}
 
 	/**
 	 * @param problem Receive a problem
 	 */
 	public ApplicationException(String problem) {
+
 		super(problem);
+
 	}
+
+	public ApplicationException(ErrorType errorType, String problem) {
+
+		super(problem);
+		this.errorType = errorType;
+
+	}
+
 }
