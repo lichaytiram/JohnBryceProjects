59233ee438ddaf2b7e2745f2f16ac5dcbfc911a7
lichay <lichaytiram@gmail.com>
2019-04-12 18:30:37 +0300

    project

--- ProjectJBv2/src/exception/ApplicationException.java
+++ ProjectJBv2/src/exception/ApplicationException.java
@@ -13,6 +13,9 @@ public class ApplicationException extends Exception {
 	private static final long serialVersionUID = -1835434097622353495L;
 
 	private ErrorType errorType;
+	private boolean isCritical;
+
+	// constructor
 
 	/**
 	 * @param problem   Receive a problem
@@ -37,10 +40,31 @@ public class ApplicationException extends Exception {
 	 * @param errorType Receive an error type
 	 * @param problem   Receive a problem
 	 */
-	public ApplicationException(ErrorType errorType, String problem) {
+	public ApplicationException(ErrorType errorType, String problem, boolean isCritical) {
 
 		super(problem);
 		this.errorType = errorType;
+		this.isCritical = isCritical;
+
+	}
+
+	// getter
+
+	/**
+	 * @return This function return the error type
+	 */
+	public ErrorType getErrorType() {
+
+		return errorType;
+
+	}
+
+	/**
+	 * @return This function return true if critical
+	 */
+	public boolean getIsCritical() {
+
+		return isCritical;
 
 	}
 
--- ProjectJBv2/src/program/Program.java
+++ ProjectJBv2/src/program/Program.java
@@ -14,6 +14,7 @@ public class Program {
 	public static void main(String[] args) {
 
 		Test.testAll();
+
 	}
 
 }
