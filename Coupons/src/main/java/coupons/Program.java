78d89899af0a09694531b8cece40d5a246b87aaf
lichay <lichaytiram@gmail.com>
2019-04-13 17:18:01 +0300

    project

--- Coupons/src/main/java/coupons/Program.java
+++ Coupons/src/main/java/coupons/Program.java
@@ -3,8 +3,6 @@ package coupons;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import coupons.program.Test;
-
 @SpringBootApplication
 public class Program {
 	public static void main(String[] args) {
