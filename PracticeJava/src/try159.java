b171e2f2305fc91130f06435300df09f7abcd8c1
Lichay Tiram <lichaytiram@gmail.com>
2019-05-20 15:07:16 +0300

    java

--- PracticeJava/src/try159.java
+++ PracticeJava/src/try159.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class try159 {
 
 	public static void main(String[] args) {
 
+		String[] arr = { "my", "name", "is", "lichay" };
+		List<String> list = new ArrayList<String>(Arrays.asList(arr));
+		list.remove("lichay");
+		list.add("tom");
+		list.add("!");
+		System.out.println(Arrays.toString(arr));
+		arr = list.toArray(new String[list.size()]);
+		System.out.println(Arrays.toString(arr));
 	}
 
 }
