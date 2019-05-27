1e1c83683dee607049af1021820eb15483a20afa
Lichay Tiram <lichaytiram@gmail.com>
2019-05-27 18:44:39 +0300

    project

--- Coupons/src/main/java/coupons/beans/ErrorBean.java
+++ Coupons/src/main/java/coupons/beans/ErrorBean.java
@@ -8,57 +8,83 @@ import coupons.enums.ErrorType;
  * @author Lichay
  *
  */
+/**
+ * @author Lichay
+ *
+ */
 public class ErrorBean {
 
 	// property
 
 	private int statusCode;
-	private ErrorType errorType;
-	private String internalMessage;
+	private ErrorType internalMessage;
+	private String externalMessage;
 
 	// constructor
 
-	public ErrorBean(int statusCode, ErrorType errorType, String internalMessage) {
-		this();
+	/**
+	 * Constructor for create a show for this class
+	 * 
+	 * @param statusCode      Receive a status code
+	 * @param internalMessage Receive an internal message
+	 * @param externalMessage Receive a external message
+	 */
+	public ErrorBean(int statusCode, ErrorType internalMessage, String externalMessage) {
+		super();
 		this.statusCode = statusCode;
-		this.errorType = errorType;
 		this.internalMessage = internalMessage;
+		this.externalMessage = externalMessage;
 	}
 
+	/**
+	 * Constructor for create a show for this class
+	 */
 	public ErrorBean() {
 		super();
 	}
 
 	// getter & setter
 
+	/**
+	 * @return This function return a status code
+	 */
 	public int getStatusCode() {
 		return statusCode;
 	}
 
-	public void setStatus(int statusCode) {
+	/**
+	 * @param statusCode Receive a status code
+	 */
+	public void setStatusCode(int statusCode) {
 		this.statusCode = statusCode;
 	}
 
-	public ErrorType getErrorType() {
-		return errorType;
-	}
-
-	public void setErrorType(ErrorType errorType) {
-		this.errorType = errorType;
-	}
-
-	public String getInternalMessage() {
+	/**
+	 * @return This function return an internal message
+	 */
+	public ErrorType getInternalMessage() {
 		return internalMessage;
 	}
 
-	public void setExternalMessage(String internalMessage) {
+	/**
+	 * @param internalMessage Receive a internal message
+	 */
+	public void setInternalMessage(ErrorType internalMessage) {
 		this.internalMessage = internalMessage;
+	}
+
+	public String getExternalMessage() {
+		return externalMessage;
+	}
+
+	public void setExternalMessage(String externalMessage) {
+		this.externalMessage = externalMessage;
 	}
 
 	@Override
 	public String toString() {
-		return "ErrorBean [statusCode=" + getStatusCode() + ", errorType=" + getErrorType() + ", internalMessage="
-				+ getInternalMessage() + "]";
+		return "ErrorBean [statusCode=" + getStatusCode() + ", internalMessage=" + getInternalMessage()
+				+ ", externalMessage=" + getExternalMessage() + "]";
 	}
 
 }
--- Coupons/src/main/java/coupons/exception/ExceptionsHandler.java
+++ Coupons/src/main/java/coupons/exception/ExceptionsHandler.java
@@ -1,7 +1,5 @@
 package coupons.exception;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
