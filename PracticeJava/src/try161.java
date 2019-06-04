b91163f456895d8add46217d2c76c2be7a8ef72a
lichaytiram <lichaytiram@gmail.com>
2019-06-05 02:16:05 +0300

    java

--- PracticeJava/src/try161.java
+++ PracticeJava/src/try161.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class try161 {
 
 	public static void main(String[] args) {
+
+		List<Integer> list = new ArrayList<Integer>(Arrays.asList(2, 4, 7, 8));
+		System.out.println(list + "\n");
+		list.forEach(p -> System.out.println(p));
+		System.out.println();
+		list.forEach(System.out::println);
 
 	}
 
